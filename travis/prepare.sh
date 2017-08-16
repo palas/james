d5e73139ab417f0f8bc8626777279e6f588eeb66
Pablo Lamela Seijas <p.lamela-seijas@kent.ac.uk>
2017-08-16 12:36:00 +0100

    Try to fix travis script

--- travis/prepare.sh
+++ travis/prepare.sh
@@ -4,6 +4,7 @@ cd temp && \
 wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && \
 sudo dpkg -i erlang-solutions_1.0_all.deb && \
 sudo apt-get update && \
+sudo apt-get install -qq emacs && \
 sudo apt-get install -qq erlang && \
 sudo apt-get install -qq tomcat7 && \
 sudo apt-get install -qq maven2 && \
